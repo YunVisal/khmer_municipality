0d5f2d969a0d12000698125a2655a0ff2db61749
kdVisalYun <yunvisal@kirirom-digital.com>
2024-09-10 17:51:42 +0900

    integrate with dropbox api

--- data_migration/main.go
+++ data_migration/main.go
@@ -2,21 +2,61 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/thedatashed/xlsxreader"
+	"io"
+	"net/http"
+	"os"
 )
 
 func main() {
-	xl, err := xlsxreader.OpenFile("../data/ncdd_admin_database_25provinces__2023.xlsx")
+	out, err := os.Create("../data/data.xlsx")
 	if err != nil {
 		fmt.Println("ERROR!")
 		fmt.Println(err)
 		return
 	}
-	defer xl.Close()
+	defer out.Close()
 
-	CreateProvinceCSVFile(xl.Sheets)
-	CreateDistrictCSVFile(xl)
-	CreateCommuneCSVFile(xl)
-	CreateVillageCSVFile(xl)
+	req, err := http.NewRequest("GET", "https://content.dropboxapi.com/2/files/download", nil)
+	if err != nil {
+		fmt.Println("ERROR!")
+		fmt.Println(err)
+		return
+	}
+	req.Header.Set("Dropbox-API-Arg", "{\"path\":\"id:qnLpP3P8qUAAAAAAAAAAHw\"}")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("ERROR!")
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Check if the request was successful
+	if resp.StatusCode != http.StatusOK {
+		fmt.Errorf("bad status: %s", resp.Status)
+		return
+	}
+
+	// Copy the response body into the file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		fmt.Println("ERROR!")
+		fmt.Println(err)
+		return
+	}
+
+	// xl, err := xlsxreader.OpenFile("../data/ncdd_admin_database_25provinces__2023.xlsx")
+	// if err != nil {
+	// 	fmt.Println("ERROR!")
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// defer xl.Close()
+
+	// CreateProvinceCSVFile(xl.Sheets)
+	// CreateDistrictCSVFile(xl)
+	// CreateCommuneCSVFile(xl)
+	// CreateVillageCSVFile(xl)
 }
