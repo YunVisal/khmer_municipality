028b05cf7f8a01122882ea24d00cc2d67b5235d3
kdVisalYun <yunvisal@kirirom-digital.com>
2024-09-11 17:11:02 +0900

    add auth and fetch file

--- data_migration/main.go
+++ data_migration/main.go
@@ -2,61 +2,87 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"html/template"
 	"net/http"
-	"os"
 )
 
+// func main() {
+// 	out, err := os.Create("../data/data.xlsx")
+// 	if err != nil {
+// 		fmt.Println("ERROR!")
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	defer out.Close()
+
+// 	req, err := http.NewRequest("GET", "https://content.dropboxapi.com/2/files/download", nil)
+// 	if err != nil {
+// 		fmt.Println("ERROR!")
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	req.Header.Set("Dropbox-API-Arg", "{\"path\":\"id:qnLpP3P8qUAAAAAAAAAAHw\"}")
+
+// 	client := &http.Client{}
+// 	resp, err := client.Do(req)
+// 	if err != nil {
+// 		fmt.Println("ERROR!")
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	defer resp.Body.Close()
+
+// 	// Check if the request was successful
+// 	if resp.StatusCode != http.StatusOK {
+// 		fmt.Errorf("bad status: %s", resp.Status)
+// 		return
+// 	}
+
+// 	// Copy the response body into the file
+// 	_, err = io.Copy(out, resp.Body)
+// 	if err != nil {
+// 		fmt.Println("ERROR!")
+// 		fmt.Println(err)
+// 		return
+// 	}
+
+// 	// xl, err := xlsxreader.OpenFile("../data/ncdd_admin_database_25provinces__2023.xlsx")
+// 	// if err != nil {
+// 	// 	fmt.Println("ERROR!")
+// 	// 	fmt.Println(err)
+// 	// 	return
+// 	// }
+// 	// defer xl.Close()
+
+// 	// CreateProvinceCSVFile(xl.Sheets)
+// 	// CreateDistrictCSVFile(xl)
+// 	// CreateCommuneCSVFile(xl)
+// 	// CreateVillageCSVFile(xl)
+// }
+
 func main() {
-	out, err := os.Create("../data/data.xlsx")
-	if err != nil {
-		fmt.Println("ERROR!")
-		fmt.Println(err)
-		return
-	}
-	defer out.Close()
+	http.HandleFunc("/", servePage)
 
-	req, err := http.NewRequest("GET", "https://content.dropboxapi.com/2/files/download", nil)
-	if err != nil {
-		fmt.Println("ERROR!")
-		fmt.Println(err)
-		return
-	}
-	req.Header.Set("Dropbox-API-Arg", "{\"path\":\"id:qnLpP3P8qUAAAAAAAAAAHw\"}")
+	port := ":8080"
+	fmt.Printf("Server running on http://localhost%s\n", port)
+	http.ListenAndServe(port, nil)
+}
 
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("ERROR!")
-		fmt.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-
-	// Check if the request was successful
-	if resp.StatusCode != http.StatusOK {
-		fmt.Errorf("bad status: %s", resp.Status)
-		return
+func servePage(w http.ResponseWriter, r *http.Request) {
+	page_path := ""
+	fmt.Println(r.URL.Path)
+	if r.URL.Path == "/" {
+		page_path = "./static/index.html"
+	} else if r.URL.Path == "/token/" {
+		page_path = "./static/token_redirect.html"
 	}
 
-	// Copy the response body into the file
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		fmt.Println("ERROR!")
-		fmt.Println(err)
-		return
+	if page_path != "" {
+		tmpl, err := template.ParseFiles(page_path)
+		if err != nil {
+			http.Error(w, "Error loading form", http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, nil)
 	}
-
-	// xl, err := xlsxreader.OpenFile("../data/ncdd_admin_database_25provinces__2023.xlsx")
-	// if err != nil {
-	// 	fmt.Println("ERROR!")
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// defer xl.Close()
-
-	// CreateProvinceCSVFile(xl.Sheets)
-	// CreateDistrictCSVFile(xl)
-	// CreateCommuneCSVFile(xl)
-	// CreateVillageCSVFile(xl)
 }
